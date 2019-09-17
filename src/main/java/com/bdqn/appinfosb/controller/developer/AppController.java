8e6ddb8d5e56c1552472d93ae234063dc546d213
xuyunfei <905231404@qq.com>
2019-09-17 08:11:36 +0800

    添加了APP应用控制器和前端页面。

---
+++ src/main/java/com/bdqn/appinfosb/controller/developer/AppController.java
@@ -0,0 +1,17 @@
+package com.bdqn.appinfosb.controller.developer;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ClassName: {@link AppController}
+ * Description: TODO APP应用控制器
+ * Author: xyf
+ * Date 2019/9/17 7:58
+ */
+@Controller
+@RequestMapping("/dev/flatform/app")
+public class AppController {
+    private Logger logger = Logger.getLogger(AppController.class);
+}
